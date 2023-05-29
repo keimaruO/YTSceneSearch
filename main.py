01d05fd3bfddc386b6cba5f63ce4b02652477aee
keimaruO <91080250+keimaruO@users.noreply.github.com>
2023-05-29 19:18:12 +0900

    Update main.py bug fix

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#version 0.1.0
+#version 0.1.1 bug fix
 
 import tkinter as tk
 import re
@@ -54,14 +54,13 @@ def search_srt(file_name, keyword1, keyword2, keyword3, search_range):
                     url = find_url(index_k3, time_code)
                     if url:
                         result.append((url, lines[index_k3].strip()))
-                        
     return result
 
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.title("字幕検索アプリ")
-        self.geometry("1000x600")
+        self.title("YTSceneSearch")
+        self.geometry("900x550")
         self.font = "Meiryo UI"
         self.configure(bg="white")
         
@@ -74,50 +73,59 @@ class App(tk.Tk):
         menu = tk.Menu(self)
         self.config(menu=menu)
         file_menu = tk.Menu(menu, tearoff=0)
-        menu.add_cascade(label="ファイル", menu=file_menu)
-        file_menu.add_command(label="開く", command=self.browse_file)
-        file_menu.add_separator()
-        file_menu.add_command(label="終了", command=self.quit)
+  
         
         # 入力・検索ボタン
         input_frame = tk.Frame(self, bg="white")
         input_frame.pack(pady=10)
         
-        tk.Label(input_frame, text="ファイルを選択:", font=(self.font, 12), bg="white").grid(row=0, column=0, pady=10)
-        tk.Entry(input_frame, textvariable=self.file_path_var, width=50, font=(self.font, 12)).grid(row=0, column=1, pady=10)
-        tk.Button(input_frame, text="参照", command=self.browse_file, font=(self.font, 12)).grid(row=0, column=2, pady=10)
+        tk.Label(input_frame, text="File Select:", font=(self.font, 11), bg="white").grid(row=0, column=0, pady=10)
+        tk.Entry(input_frame, textvariable=self.file_path_var, width=50, font=(self.font, 11)).grid(row=0, column=1, pady=10)
+        tk.Button(input_frame, text="Browse", command=self.browse_file, font=(self.font, 11)).grid(row=0, column=2, pady=10)
         
-        self.search_word1_entry = tk.Entry(input_frame, textvariable=self.search_word1, width=50, font=(self.font, 12))
+        self.search_word1_entry = tk.Entry(input_frame, textvariable=self.search_word1, width=50, font=(self.font, 11))
         self.search_word1_entry.grid(row=1, column=1, pady=10)
         self.search_word1_entry.bind("<Return>", self.focus_next)
 
-        self.search_word2_entry = tk.Entry(input_frame, textvariable=self.search_word2, width=50, font=(self.font, 12))
+        self.search_word2_entry = tk.Entry(input_frame, textvariable=self.search_word2, width=50, font=(self.font, 11))
         self.search_word2_entry.grid(row=2, column=1, pady=10)
         self.search_word2_entry.bind("<Return>", self.focus_next)
 
-        self.search_word3_entry = tk.Entry(input_frame, textvariable=self.search_word3, width=50, font=(self.font, 12))
+        self.search_word3_entry = tk.Entry(input_frame, textvariable=self.search_word3, width=50, font=(self.font, 11))
         self.search_word3_entry.grid(row=3, column=1, pady=10)
         self.search_word3_entry.bind("<Return>", self.focus_next)
 
-        tk.Button(input_frame, text="検索", command=self.search_word, font=(self.font, 12)).grid(row=4, column=1, pady=10)
+        tk.Button(input_frame, text="Search", command=self.search_word, font=(self.font, 11)).grid(row=4, column=1, pady=10)
 
         # 出力
         output_frame = tk.Frame(self, bg="white")
         output_frame.pack(padx=50, pady=10, fill="both", expand=True)
 
-        self.output_text = tk.Text(output_frame, wrap="word", font=(self.font, 12), state="disabled")
-        self.output_text.pack(pady=10, fill="both", expand=True)
 
-        scrollbar = tk.Scrollbar(output_frame, command=self.output_text.yview, width=20)
-        scrollbar.pack(side="right", fill="y")
-        self.output_text.configure(yscrollcommand=scrollbar.set)
+        self.output_text = tk.Text(output_frame, wrap="none", font=(self.font, 11), state="disabled")
+        self.output_text.grid(row=0, column=0, sticky='nsew')
+
+        y_scrollbar = tk.Scrollbar(output_frame, command=self.output_text.yview, width=20)
+        y_scrollbar.grid(row=0, column=1, sticky='ns')
+
+        x_scrollbar = tk.Scrollbar(output_frame, command=self.output_text.xview, orient="horizontal", width=20)
+        x_scrollbar.grid(row=1, column=0, sticky='ew')
+
+        self.output_text.configure(yscrollcommand=y_scrollbar.set, xscrollcommand=x_scrollbar.set)
+
+        output_frame.grid_columnconfigure(0, weight=1)
+        output_frame.grid_rowconfigure(0, weight=1)
+
+
+
+        self.output_text.configure(yscrollcommand=y_scrollbar.set, xscrollcommand=x_scrollbar.set)
 
 
         self.output_text.tag_config("link", foreground="blue", underline=1)
         self.output_text.bind("<Button-1>", self.on_click)
 
 
-        self.load_label = tk.Label(input_frame, text="Now Loading...", font=(self.font, 12), fg="red", bg="white")
+        self.load_label = tk.Label(input_frame, text="Now Loading...", font=(self.font, 11), fg="red", bg="white")
         
 
         self.bind("<Configure>", self.on_resize)
@@ -233,20 +241,26 @@ class App(tk.Tk):
     
     def search_srt_from_gui(self, search_groups, search_range):
         file_name = self.file_path_var.get()
-
+    
         self.output_text.configure(state="normal")
         self.output_text.delete("1.0", "end")
-
+    
+        displayed_urls = set()
+    
         for combination in itertools.product(*search_groups):
             search_results = search_srt(file_name, *combination, search_range)
-
+    
             for result in search_results:
                 url, line = result
-                self.output_text.insert("end", url, "link")
-                self.output_text.insert("end", "\n" + line + "\n\n")
 
+                if url not in displayed_urls:
+                    self.output_text.insert("end", url, "link")
+                    self.output_text.insert("end", "\n" + line + "\n\n")
+
+                    displayed_urls.add(url)
+    
         self.output_text.configure(state="disabled")
-
+        self.after(0, self.load_label.grid_remove)
 
 if __name__ == "__main__":
     app = App()
